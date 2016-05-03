763c590ff5a1e2aaa9cb727ffea5c827e3d413bb
mushipa <dryadg79585@gmail.com>
2016-05-03 10:28:29 +0800

    edit header
    加入論壇分類

--- resources/views/layouts/_header.blade.php
+++ resources/views/layouts/_header.blade.php
@@ -2,7 +2,9 @@
     <div class="nav">
         <a href="{{env('APP_URL')}}"><img class = "logo-pic" src="/img/logo.png" /></a>
         <div class = "category-bar">
-
+            <div class="category-item">
+            <a href = "{{route("forum")}}">2016異想論壇</a>
+            </div>
             @foreach ($categories as $category)
                 <div class="category-item">
                     <a href = "{{ route('category', ['category' => $category->name])}} ">{{$category->name}}</a>
