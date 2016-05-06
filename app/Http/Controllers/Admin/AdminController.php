33b35184c9bd3fa614a49fd218540cd0521f1a4c
Charlie Hsu <hsu21609@gmail.com>
2016-05-06 08:56:13 +0800

    add admin index page

--- app/Http/Controllers/Admin/AdminController.php
+++ app/Http/Controllers/Admin/AdminController.php
@@ -19,7 +19,7 @@ class AdminController extends Controller
     }
 
 	public function index(){
-    	return redirect()->route('article.index');
+    	return view('admin.index');
 	}
 
 	public function login(){
---
+++ resources/views/admin/index.blade.php
@@ -0,0 +1,7 @@
+@extends('admin.layouts.master')
+
+@section('content')
+    <h1>
+        歡迎使用 Besides 網站管理系統
+    </h1>
+@endsection
