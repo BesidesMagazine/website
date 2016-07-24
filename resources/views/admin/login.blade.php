e6db73563f6d36a1f04941b5640ca558edfea511
Charlie Hsu <hsu21609@gmail.com>
2016-07-24 23:38:06 +0800

    new login view

--- resources/views/admin/login.blade.php
+++ resources/views/admin/login.blade.php
@@ -2,16 +2,21 @@
 <html>
     @include('admin.layouts.head')
     <body>
-        <div class="container">
+        <div class="container" style="margin-top:100px;">
             <div class="col-md-6 col-md-offset-3">
+                <img src="/img/translogo.png" style="display:inline-block;padding:10px;">
                 <div class="panel panel-default">
                     <div class="panel-body">
-                        <h3>請輸入密碼</h3>
                         <form action="{{ route('authenticate') }}" method="post">
+                            <div class="form-group">
+                                <label>請輸入密碼</label>
+                            </div>
                             <div class="form-group">
                                 <input class="form-control" type="password" name="password" id="password" value="">
                             </div>
-                            <button type="submit" class="btn btn-primary">submit</button>
+                            <div class="form-group">
+                                <button type="submit" class="btn btn-default">submit</button>
+                            </div>
                             <input type="hidden" name="_token" value="{{ csrf_token() }}">
                         </form>
                     </div>
