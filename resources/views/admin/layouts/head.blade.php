cc330ae73257d7ec169a34739c179c002ffae42a
Charlie Hsu <hsu21609@gmail.com>
2016-07-24 17:04:28 +0800

    rewrite login page

---
+++ resources/views/admin/layouts/head.blade.php
@@ -0,0 +1,6 @@
+<head>
+    <title>@yield('title')</title>
+    <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.6/css/bootstrap.min.css" integrity="sha384-1q8mTJOASx8j1Au+a5WDVnPi2lkFfwwEAa8hDDdjZlpLegxhjVME1fgjWPGmkzs7" crossorigin="anonymous">
+    <script src="https://code.jquery.com/jquery-2.2.2.min.js" integrity="sha256-36cp2Co+/62rEAAYHLmRCPIych47CvdM+uTBJwSzWjI=" crossorigin="anonymous"></script>
+    <script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.6/js/bootstrap.min.js" integrity="sha384-0mSbJDEHialfmuBBQP6A4Qrprq5OVfW37PRR3j5ELqxss1yVqOtnepnHVP9aJ7xS" crossorigin="anonymous"></script>
+</head>
--- resources/views/admin/login.blade.php
+++ resources/views/admin/login.blade.php
@@ -1,17 +1,22 @@
-@extends('admin.layouts.master')
-
-@section('content')
-    <div class="row">
-        <div class="col-md-6">
-            <h3>Login</h3>
-            <form action="{{ route('authenticate') }}" method="post">
-                <div class="form-group">
-                    <label for="password">Your Password</label>
-                    <input class="form-control" type="password" name="password" id="password" value="">
+<!DOCTYPE html>
+<html>
+    @include('admin.layouts.head')
+    <body>
+        <div class="container">
+            <div class="col-md-6 col-md-offset-3">
+                <div class="panel panel-default">
+                    <div class="panel-body">
+                        <h3>請輸入密碼</h3>
+                        <form action="{{ route('authenticate') }}" method="post">
+                            <div class="form-group">
+                                <input class="form-control" type="password" name="password" id="password" value="">
+                            </div>
+                            <button type="submit" class="btn btn-primary">submit</button>
+                            <input type="hidden" name="_token" value="{{ csrf_token() }}">
+                        </form>
+                    </div>
                 </div>
-                <button type="submit" class="btn btn-primary">submit</button>
-                <input type="hidden" name="_token" value="{{ csrf_token() }}">
-            </form>
+            </div>
         </div>
-    </div>
-@endsection
+    </body>
+</html>
