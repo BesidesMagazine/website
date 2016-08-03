4044337cd1a1c06e890be44167f80fe419c94a5e
Charlie Hsu <hsu21609@gmail.com>
2016-08-04 03:39:08 +0800

    new author show

--- resources/views/author/show.blade.php
+++ resources/views/author/show.blade.php
@@ -1,21 +1,23 @@
 @extends('layouts.master')
 
 @section('content')
-	<div style="height:50px;"></div>
-	<dir style="text-align:left;">
-		<div class="author-title" style="background-image: url('{{$author->image_url}}');height:320px;width:320px;
-	    background-position: center;
-	    background-size: cover;display: inline-block;">
-		</div>
-		<div style="margin-left:60px;height:320px;display: inline-block;vertical-align:top;max-width:500px">
-			<h1 style="display: inline-block;letter-spacing: 0.2em;">{{$author->name}}</h1>
-			<hr>
-			<p style="display: inline-block;">{{$author->introduction}}</p>
-		</div>
-	</dir>
+    <div class="content-container" style="margin-top:50px;">
+    	<dir style="text-align:left;">
+    		<div class="card" style="background-image: url('{{$author->image_url}}');height:320px;width:320px;
+    	    background-$main-content-widthion: center;
+    	    background-size: cover;display: inline-block;">
+    		</div>
+    		<div class="card"  style="margin-left:60px;height:320px;display: inline-block;vertical-align:top;max-width:500px">
+    			<span class="article-title" >{{$author->name}}</span>
+    			<hr class="separator">
+    			<p style="display: inline-block;">{{$author->introduction}}</p>
+    		</div>
+    	</dir>
 
-	<hr class="separator" style="width: 95%;margin-bottom:20px;">
-	@foreach ($articles as $article)
-      @include('partials._card')
-    @endforeach
+    	<hr class="separator" style="width: 95%;margin-bottom:20px;">
+    	@foreach ($articles as $article)
+          @include('partials._card')
+        @endforeach
+    </div>
+
 @endsection
